15c4039312ef9429d0a846bc41c8e5a7767d9dea
Janpaou <122214811+Janpaou@users.noreply.github.com>
2025-08-02 10:19:29 +0200

    Update fantacalcio.py
    aggiunto tkinter per gui

--- fantacalcio.py
+++ fantacalcio.py
@@ -1,27 +1,35 @@
+import tkinter as tk
+from tkinter import ttk
 import pandas as pd
 
 def calcola_valore_portiere(row):
-    return (
+    return round((
         row["Mv"] * row["Pv"]+
         row["Rp"] * 3 -
         row["Gs"] * 1 -
         row["Amm"] * 0.5 -
         row["Esp"] * 2 -
         row["Au"] * 3
-    ) * get_quotazione(row["Id"]) /1000
+    ) * get_quotazione(row["Id"]) /1000, 2)
 
 def calcola_valore_mov(row):
-    return (
+    return round((
         row["Mv"] * row["Pv"]+
         (row["Gf"] - row["R-"])* 3 -
         row["Amm"] * 0.5 -
         row["Esp"] * 2 -
         row["Au"] * 3 +
         row["Ass"] * 1
-    ) * get_quotazione(row["Id"]) /1000
+    ) * get_quotazione(row["Id"]) /1000, 2)
 
 def get_giocatori(per_ruolo):
-    df = pd.read_excel("Statistiche_Fantacalcio_Stagione_2024_25.XLSX", header=1)
+    df_stat = pd.read_excel("Statistiche_Fantacalcio_Stagione_2024_25.XLSX", header=1)
+    df_quot = pd.read_excel("Quotazioni_Fantacalcio_Stagione_2025_26.xlsx", header=1)
+    df = pd.merge(df_stat, df_quot[["Id", "R"]], on="Id", how="outer", suffixes=("", "_nuovo"))
+    df["R"] = df["R_nuovo"]
+
+    # Rimuovi la colonna temporanea
+    df= df.drop(columns=["R_nuovo"])
 
     # Filtra i ruoli ufficiali se specificato
     if per_ruolo:
@@ -49,7 +57,7 @@ def get_quotazione(player_id):
 def normalizza_valori(df, minimo, massimo):
     min_val = df["Valore"].min()
     max_val = df["Valore"].max()
-    df["Valore" + "_norm"] = ((df["Valore"] - min_val) / (max_val - min_val)) * (massimo - minimo) + minimo
+    df["Valore" + "_norm"] = round(((df["Valore"] - min_val) / (max_val - min_val)) * (massimo - minimo) + minimo, 2)
     return df
 
 
@@ -57,9 +65,9 @@ def normalizza_valori(df, minimo, massimo):
 portieri = get_giocatori("P")
 portieri = normalizza_valori(portieri, 1, 100)
 difensori = get_giocatori("D")
-difensori = normalizza_valori(difensori, 1, 110)
+difensori = normalizza_valori(difensori, 1, 90)
 centrocampisti = get_giocatori("C")
-centrocampisti = normalizza_valori(centrocampisti, 1, 130)
+centrocampisti = normalizza_valori(centrocampisti, 1, 160)
 attaccanti = get_giocatori("A")
 attaccanti = normalizza_valori(attaccanti, 1, 320)
 #print(portieri[["Nome", "R", "Mv", "Rp", "Gs", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False).head(20))
@@ -68,81 +76,85 @@ attaccanti = normalizza_valori(attaccanti, 1, 320)
 #print(attaccanti[["Nome", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False).head(30))
 
 
-def menu():
-    print("Cosa vuoi fare?")
-    print("1. Cerca un giocatore per nome")
-    print("2. Cerca giocatori per squadra")
-    print("3. Visualizza giocatori per ruolo (P, D, C, A)")
-    print("4. Visualizza tutti i ruoli")
-    scelta = input("Inserisci la tua scelta (1, 2, 3): ")
+def menu(scelta):
 
-    if scelta == "1":
-        nome = input("Inserisci il nome del giocatore: ").strip().lower()
-        all_players = pd.concat([portieri, difensori, centrocampisti, attaccanti])
-        risultati = all_players[all_players["Nome"].str.lower().str.contains(nome)]
+    match scelta:
+        case "p":
+            pd.set_option("display.max_rows", None)
+            mostra_risultati(portieri[["Nome","Squadra", "R", "Mv", "Rp", "Gs", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
+            
+        case "d":
+            pd.set_option("display.max_rows", None)
+            mostra_risultati(difensori[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
+            
+        case "c":
+            pd.set_option("display.max_rows", None)
+            mostra_risultati(centrocampisti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
 
-        if risultati.empty:
-            print("Nessun giocatore trovato.")
-            menu()
-        else:
-            print("")
-            print(risultati[["Nome", "R", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
-            print("")
-            print("")
-            menu()
-    elif scelta == "2":
-        sqaudra = input("Inserisci il nome della squadra: ").strip().lower()
-        all_players = pd.concat([portieri, difensori, centrocampisti, attaccanti])
-        risultati = all_players[all_players["Squadra"].str.lower().str.contains(sqaudra)]
+        case "a":
+            pd.set_option("display.max_rows", None)
+            mostra_risultati(attaccanti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
 
-        if risultati.empty:
-            print("Nessun giocatore trovato.")
-            menu()
-        else:
-            print("")
-            print(risultati[["Nome", "R", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
-            print("")
-            print("")
-            menu()
+        case "tutti":
+            mostra_risultati("Portieri:")
+            print(portieri[["Nome","Squadra", "R", "Mv", "Rp", "Gs", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False).head(20))
+            print("\nDifensori:")
+            mostra_risultati(difensori[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False).head(30))
+            print("\nCentrocampisti:")
+            mostra_risultati(centrocampisti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False).head(30))
+            print("\nAttaccanti:")
+            mostra_risultati(attaccanti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False).head(30))
 
-    elif scelta == "3":
-        ruolo = input("Inserisci il ruolo (P, D, C, A): ").upper()
-        pd.set_option("display.max_rows", None)
+        case _:
+            all_players = pd.concat([portieri, difensori, centrocampisti, attaccanti])
+            risultati = all_players[all_players["Squadra"].str.lower().str.contains(scelta, na=False)]
 
-        if ruolo == "P":
-            print(portieri[["Nome","Squadra", "R", "Mv", "Rp", "Gs", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
-            print("")
-            print("")
-            menu()
-        elif ruolo == "D":
-            print(difensori[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False))
-            print("")
-            print("")
-            menu()
-        elif ruolo == "C":
-            print(centrocampisti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False))
-            print("")
-            print("")
-            menu()
-        elif ruolo == "A":
-            print(attaccanti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False))
-            print("")
-            print("")
-            menu()
-        else:
-            print("Ruolo non valido. Usa solo P, D, C o A.")
+            if risultati.empty:
+                risultati = all_players[all_players["Nome"].str.lower().str.contains(scelta, na=False)]
+                mostra_risultati(risultati[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
+            else:
+                mostra_risultati(risultati[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False))
 
-    elif scelta == "4":
-        print("Portieri:")
-        print(portieri[["Nome","Squadra", "R", "Mv", "Rp", "Gs", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore_norm", ascending=False).head(20))
-        print("\nDifensori:")
-        print(difensori[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False).head(30))
-        print("\nCentrocampisti:")
-        print(centrocampisti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False).head(30))
-        print("\nAttaccanti:")
-        print(attaccanti[["Nome","Squadra", "R", "Mv", "Gf", "Ass", "Amm", "Esp", "Au", "Valore", "Valore_norm"]].sort_values("Valore", ascending=False).head(30))
+
+def mostra_risultati(risultati):
+    tree.delete(*tree.get_children())
+    tree["columns"] = list(risultati.columns)
+    search_entry.delete(0, tk.END)
+    if risultati.empty:
+        tree.insert(tk.END, "Nessun risultato trovato.")
     else:
-        print("Scelta non valida.")
+        for col in risultati.columns:
+            tree.heading(col, text=col)
+            tree.column(col, width=100, anchor="center")  # personalizza larghezza
 
-# Avvia il menu
-menu()
+    # Inserisci righe
+        for _, row in risultati.iterrows():
+            tree.insert("", "end", values=list(row))
+
+
+if __name__ == "__main__":
+    try:
+
+        root = tk.Tk()
+        root.title("Ricerca Giocatori Fantacalcio")
+        root.geometry("800x600")
+
+        # Entry per ricerca
+        search_entry = tk.Entry(root, width=50)
+        search_entry.pack(pady=10)
+        search_entry.bind("<Return>", lambda event: menu(search_entry.get().lower()))
+        search_entry.focus_set()
+
+        # Area risultati
+        #text = tk.Text(root, wrap="none", width=100, height=25)
+        #text.pack()
+
+        tree = ttk.Treeview(root, columns=[], show="headings")
+        tree.pack(fill="both", expand=True)
+
+        tk.Button(root, text="Cerca", command=lambda: menu(search_entry.get().lower())).pack()
+        
+        #menu()
+    except Exception as e:
+        print("Errore:", e)
+    input("\nPremi invio per chiudere...")
